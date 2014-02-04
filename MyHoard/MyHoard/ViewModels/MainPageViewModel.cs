8b0716538d0cceda1e82ee78f7844b0badb77769
Szymon Cywinski <sz.cywinski@gmail.com>
2014-02-04 15:43:53 +0100

    initial commit

---
+++ MyHoard/MyHoard/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHoard.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+
+        }
+    }
+}
